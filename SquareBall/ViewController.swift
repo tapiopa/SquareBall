6ebc7dc85a1288c77c8b09d571431bd585e4c6aa
tapiopa <tapiopa@gmail.com>
2017-09-13 10:25:43 +0300

    Initial Commit

---
+++ SquareBall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SquareBall
+//
+//  Created by Työ on 13.9.2017.
+//  Copyright © 2017 Tapio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
